b2714dda6e2491854c5e1d676848f39dd27183a8
Andis-08 <bakare.an@northeastern.edu>
2025-10-12 18:17:30 -0400

    Dynamic Styling, Parameterizing Components, Rendering Data Structure

--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -23,8 +23,17 @@ import Spreader from "./Spreader";
 import Destructing from "./Destructing";
 import FunctionDestructing from "./FunctionDestructing";
 import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import Add from "./Add";
+import Square from "./Square";
+import Highlight from "./Highlight";
+import PathParameters from "./PathParameters";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
 
 export default function Lab3() {
+  console.log("Hello World!");
   return (
     <div id="wd-lab3">
       <h3>Lab 3</h3>
@@ -53,6 +62,24 @@ export default function Lab3() {
       <Destructing />
       <FunctionDestructing />
       <DestructingImports />
+      <Classes />
+      <Styles />
+      <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+        consectetur, sed libero dolorum vitae placeat ipsam autem facere magni
+        cupiditate ex magnam deserunt non quidem, possimus nam quo, culpa
+        numquam! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Dolorem voluptatibus, blanditiis rem voluptate aspernatur ut cupiditate
+        mollitia iure quis, quia cumque ipsa quos tenetur facilis. Mollitia
+        sapiente minima optio et.
+      </Highlight>
+      <PathParameters />
+      <TodoItem />
+      <TodoList />
     </div>
   );
 }
